36d996e6c1231e9e5c3f170cb600bb255db2ffba
Sujoy Anchan <sujoy259@gmail.com>
2024-03-25 17:46:16 +0530

    Update NavBar.js

--- client/NavBar.js
+++ client/NavBar.js
@@ -1,19 +1,18 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function NavBar(){
+export default function NavBar() {
 
     const un = localStorage.getItem("un");
 
-return(
-    <>
-    <center>
-    <div className='nav'>
-        {(un==null) && (<Link to='/'>Order</Link>)}
-        {(un==null) && (<Link to='/admin'>Cafe Admin Login</Link>)}
-        {(un != null) && ( <Link to="/home"> Home </Link>) }
-    </div>
-    </center>
-    </>
+    return (
+        <>
+            <center>
+                <div className='nav'>
+                    {(un == null) && (<Link to='/'>Order</Link>)}
+                    {(un == null) && (<Link to='/admin'>Cafe Admin Login</Link>)}
+                </div>
+            </center>
+        </>
 
-);
+    );
 }
